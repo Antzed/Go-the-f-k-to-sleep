5a027025e88d784b5f0df1c098de194df4c1d33e
Anthony <iceearzhang@outlook.com>
2022-03-10 15:54:47 -0800

    improved logging.sh and reformated anthony2022.md

--- logging.sh
+++ logging.sh
@@ -8,10 +8,10 @@ varcurrenttime=$(date "+%I:%M%p")
 currenttime=$(date +%H:%M)
    if [[ "$currenttime" > "24:00" ]] || [[ "$currenttime" < "06:00" ]]; 
    then
-	   vardate=$(date -d "yesterday" "+%Y/%m/%d")
+	   vardate=$(date -d "yesterday" "+%Y/%m/%d %a")
 	   printf "\n$vardate 次日$varcurrenttime $varreason\n" >> Anthony/anthony2022.md
    else
-	   vardate=$(date "+%Y/%m/%d")
+	   vardate=$(date "+%Y/%m/%d %a")
 	   printf "\n$vardate $varcurrenttime $varreason\n" >> Anthony/anthony2022.md
 
    fi
